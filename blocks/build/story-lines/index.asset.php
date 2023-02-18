78b4543390f446c5f7c18285615565f49e1bcceb
Jacob Martella <me@jacobmartella.com>
2023-02-18 16:39:19 -0600

    Started working on updating the block.
    Addresses #18

---
+++ blocks/build/story-lines/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '8c90b60553c07ba345bb');
---
+++ blocks/story-lines/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '81a2fa511ef2c0823f6e');
